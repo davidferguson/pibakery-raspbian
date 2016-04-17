2aa153bd2253295b51386d2b912b8c7d861d7893
David Ferguson <davidferguson@users.noreply.github.com>
2016-04-17 17:10:04 +0100

    initial commit of pibakery launchers
    these files are the extras that have been added to a standard raspbian image to run PiBakery

---
+++ opt/PiBakery/removeNext.py
@@ -0,0 +1,16 @@
+# Removes the first boot blockly xml from /boot/PiBakery/blocks.xml 
+
+from xml.dom import minidom
+
+xmldoc = minidom.parse('/boot/PiBakery/blocks.xml')
+root = xmldoc.documentElement
+
+blocks = xmldoc.getElementsByTagName('block')
+for block in blocks:
+	if block.hasAttribute('type'):
+		if block.getAttribute('type') == 'onnextboot':
+			root.removeChild(block)
+
+blockFile = open('/boot/PiBakery/blocks.xml','wb')
+root.writexml(blockFile)
+blockFile.close()
